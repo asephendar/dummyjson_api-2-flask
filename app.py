fe782b47b99d2a0b261871e136c836af288ec788
Asep Hendar <4sep.hendar@gmail.com>
2024-03-06 22:10:30 +0700

    Membuat parameter query dari method GET

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import requests
 
 app = Flask(__name__)
@@ -17,6 +17,17 @@ def search_products(id):
     response = requests.get(f"{url}/{id}")
     data = response.json()
 
+    return data
+
+@app.route("/s", methods=['GET'])
+def view_search_products():
+    limit = request.args.get("limit")
+    skip = request.args.get("skip")
+    select = request.args.get("select")
+
+    response = requests.get(f"{url}?limit={limit}&skip={skip}&select={select}")
+    data = response.json()
+    
     return data
 
 if __name__ == "__main__":
